53e3c467a91e21d06e4d5579fc7413ec65bee346
Raphael <blefari_101@hotmail.com>
2018-04-05 01:47:12 -0300

    Adicionando Estrutura usuario em Minha-Conta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MinhaContaComponent } from './minha-conta/minha-conta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'conta', component: MinhaContaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
